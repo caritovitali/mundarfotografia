4f061832eda78f3a04691028b6ab0b89e491f82b
vitali.carolina <caritovitali@gmail.com>
2021-12-26 15:16:40 -0300

    cambios finales

--- js/main.js
+++ js/main.js
@@ -6,9 +6,9 @@
         if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
           document.getElementById("navbar").style.padding = "0px 0px";
           document.getElementById("navbar").style.backgroundColor = "#3e6481";
-          document.getElementById("logo").style.width = "50px";
+          document.getElementById("logo").style.width = "45px";
         } else {
-          document.getElementById("navbar").style.padding = "10px 5px";
+          document.getElementById("navbar").style.padding = "0px 0px";
           document.getElementById("navbar").style.backgroundColor = "#3e64816b";
           document.getElementById("logo").style.width = "70px";
         }
